4a632b7bcab2b03e10819bc04da30383fc9d133a
0-omkar <ovarpe2@gmail.com>
2026-07-23 11:27:59 +0530

    subdomain error in cloudflare link resolved

--- lib/r2.ts
+++ lib/r2.ts
@@ -16,6 +16,7 @@ function getR2Client() {
   const s3Client = new S3Client({
     region: 'auto',
     endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
+    forcePathStyle: true,
     credentials: {
       accessKeyId,
       secretAccessKey,
